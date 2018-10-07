124cc7a901aa46dcd137d049d430066055cd0b69
Nidhi Gaday <nidhi.gaday@groupbyinc.com>
2018-10-07 02:59:28 -0400

    component for navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,8 +1,27 @@
 import * as React from "react";
 import "./index.css";
 
+import * as Layout from "../layout/components";
+
+const navigation = [
+  { label: "Home", url: "#" },
+  { label: "Work", url: "#" },
+  { label: "Project", url: "#" },
+  { label: "Skills", url: "#" },
+  { label: "About", url: "#" },
+  { label: "Contact", url: "#" }
+];
+
 const Navigation = () => {
-  return <p>Nav 1</p>;
+  return (
+    <Layout.Navigation classes="root">
+      <Layout.Tabs>
+        {navigation.map(({ label, url }) => (
+          <Layout.Tab url={url}>{label}</Layout.Tab>
+        ))}
+      </Layout.Tabs>
+    </Layout.Navigation>
+  );
 };
 
 export default Navigation;
