6c08bfdd3a9fa48460217e05cf7e94d6bbf43326
Nidhi Gaday <nidhi.gaday@groupbyinc.com>
2018-10-07 03:29:24 -0400

    layout class changes

--- src/layout/components.js
+++ src/layout/components.js
@@ -4,36 +4,55 @@ import * as classNames from "classnames";
 
 import "./main.css";
 
-export const Wrapper = ({ children }) => {
-  return <div className="wrapper">{children}</div>;
+export const Wrapper = ({ classes, children }) => {
+  return <div className={classNames("wrapper", classes)}>{children}</div>;
 };
 
-export const Container = ({ children }) => {
-  return <div className="container">{children}</div>;
+export const Container = ({ classes, children }) => {
+  return <div className={classNames("container", classes)}>{children}</div>;
 };
 
 export const Navigation = ({ classes, children }) => {
   return <div className={classNames("nav", classes)}>{children}</div>;
 };
 
-export const Tabs = ({ children }) => {
-  return <ul className="tabs">{children}</ul>;
+export const Tabs = ({ classes, children }) => {
+  return <ul className={classNames("tabs", classes)}>{children}</ul>;
 };
 
-export const Tab = ({ url, openIn, children }) => {
+export const Tab = ({ linkClass, tabClass, url, openIn, children }) => {
   return (
-    <li className="tab">
-      <a className="navLink" href={url} target={openIn}>
+    <li className={classNames("tab", tabClass)}>
+      <a
+        className={classNames("navLink", linkClass)}
+        href={url}
+        target={openIn}
+      >
         {children}
       </a>
     </li>
   );
 };
 
-export const Row = ({ children }) => {
-  return <div className="row">{children}</div>;
+export const Row = ({ classes, children }) => {
+  return <div className={classNames("row", classes)}>{children}</div>;
 };
 
-// export const Columns = ({ ...type, children }) => {
-//   return <div className={classNames(type)}>{children}</div>;
-// };
+export const Column = ({ classes, children }) => {
+  return <div className={classNames("column", classes)}>{children}</div>;
+};
+
+export const Header = ({
+  title,
+  subtitle,
+  metadata,
+  caption,
+  label,
+  classes,
+  children
+}) => {
+  return <React.Fragment>{title && <h1>{children}</h1>}</React.Fragment>;
+};
+export const Footer = ({ classes, children }) => {
+  return <div className={classNames("footer", ...classes)}>{children}</div>;
+};
